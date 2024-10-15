e9a1f6f0e275646895c4dd7a97840c3f9b135af0
vanliflo <florian.van-liedekerke@univ-rouen.fr>
2024-10-15 15:30:40 +0200

    fix: push CategoryRepository

--- backend/src/main/java/org/example/tp1/repositories/CategoryRepository.java
+++ backend/src/main/java/org/example/tp1/repositories/CategoryRepository.java
@@ -3,6 +3,9 @@ package org.example.tp1.repositories;
 import org.example.tp1.entities.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Integer> {
 
+    Optional<Category> findByName(String name);
 }
