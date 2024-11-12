8359d692a1b9e8c79fe58dda1a2bf6567312018e
Erwann <montagne.erwann.2@gmail.com>
2024-11-12 18:37:57 +0100

    fix: fix page count when click on home button

--- frontend/src/app/category-list/category-list.component.ts
+++ frontend/src/app/category-list/category-list.component.ts
@@ -23,11 +23,16 @@ export class CategoryListComponent implements OnInit {
     this.categoryService.currentCategories.subscribe(
       value => this.categories = value
     );
-    this.categoryService.pageCount.subscribe(
+    this.categoryService.pages.subscribe(
       value => this.pageCount = value
     )
-    this.parentName = 'None';
-    this.currentPage = 0
+    this.categoryService.currentPage.subscribe(
+      value => this.currentPage = value
+    )
+    this.categoryService.lastParentName.subscribe(value => {
+      this.parentName = value
+    })
+    this.currentPage = this.categoryService.currentPage.value;
   }
 
   ngOnInit() {
@@ -39,34 +44,38 @@ export class CategoryListComponent implements OnInit {
       this.categories = Object.values(data);
     });
     this.categoryService.findPageCategoriesCount(this.parentName).subscribe(data => {
-      this.pageCount = data.id;
+      this.pageCount = data;
+      this.categoryService.pages.next(data);
     })
+    this.categoryService.currentPage.subscribe(value =>
+        this.currentPage = value,
+    )
   }
 
   changeParentCategory(category: Category) {
     this.parentName = category.name;
-    this.currentPage = 0;
+    this.categoryService.currentPage.next(0);
     this.fetchData();
     this.categoryService.lastParentName.next(category.name);
   }
 
   displayFirstPage() {
-    this.currentPage = 0
+    this.categoryService.currentPage.next(0);
     this.fetchData();
   }
 
   displayLastPage() {
-    this.currentPage = this.pageCount - 1;
+    this.categoryService.currentPage.next(this.pageCount - 1);
     this.fetchData();
   }
 
   displayPreviousPage() {
-    this.currentPage = this.currentPage - 1;
+    this.categoryService.currentPage.next(this.currentPage - 1);
     this.fetchData();
   }
 
   displayNextPage() {
-    this.currentPage = this.currentPage + 1;
+    this.categoryService.currentPage.next(this.currentPage + 1);
     this.fetchData();
   }
 
