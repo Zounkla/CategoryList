52d2fd8463e8c44b571dd33d4c3bd2bbbdf79a51
vanliflo <florian.van-liedekerke@univ-rouen.fr>
2024-12-06 14:40:38 +0100

    fix(): start to refactor backend routes

---
+++ backend/src/main/java/org/example/tp1/examples/JsonExample.java
@@ -0,0 +1,30 @@
+package org.example.tp1.examples;
+
+public class JsonExample {
+ public final static String CATEGORIES_EXAMPLE =
+            "{\n" +
+            "    \"categories\": {\n" +
+            "        \"DC\": {\n" +
+            "            \"children\": \"[Batman, Superman]\",\n" +
+            "            \"date\": \"2024-12-04 09:19:48.176\",\n" +
+            "            \"id\": \"1\",\n" +
+            "            \"name\": \"DC\",\n" +
+            "            \"parent\": \"null\"\n" +
+            "        },\n" +
+            "        \"Batman\": {\n" +
+            "            \"children\": \"[]\",\n" +
+            "            \"date\": \"2024-12-04 09:19:48.187\",\n" +
+            "            \"id\": \"2\",\n" +
+            "            \"name\": \"Batman\",\n" +
+            "            \"parent\": \"DC\"\n" +
+            "        },\n" +
+            "        \"Superman\": {\n" +
+            "            \"children\": \"[]\",\n" +
+            "            \"date\": \"2024-12-04 09:19:48.187\",\n" +
+            "            \"id\": \"3\",\n" +
+            "            \"name\": \"Superman\",\n" +
+            "            \"parent\": \"DC\"\n" +
+            "        },\n" +
+            "        \"pageCount\": \"2\"\n" +
+            "    }\n";
+}
